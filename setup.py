10a8cd75000c2ca15386b62a1ed8106545370b48
alex wennerberg <awennerberg@cloudbakers.com>
2019-01-18 17:14:44 -0600

    bumping version

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ test_requirements = ["pytest"]
 
 setup(
     name="agm",
-    version="0.1.0",
+    version="0.1.1",
     license="Apache",
     author="Alex Wennerberg",
     author_email="alex@alexwennerberg.com",
