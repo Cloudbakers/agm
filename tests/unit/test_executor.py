397ff403bfdf10d162d08f136a7c01813731ad2d
alex wennerberg <awennerberg@cloudbakers.com>
2019-01-19 13:37:30 -0600

    Wrote basic API request tests

--- tests/unit/test_executor.py
+++ tests/unit/test_executor.py
@@ -1,3 +1,24 @@
-# TODO: Write tests
-def test_one():
+import pytest
+from agm import executor
+
+
+@pytest.fixture
+def sample_api_request():
+    request = executor.GoogleAPIRequest(
+        "service", ["a", "b"], "method", "v1", sample_field="test"
+    )
+    return request
+
+
+def test_basic_parameters(sample_api_request):
+    assert sample_api_request.service == "service"
+    assert sample_api_request.resources == ["a", "b"]
+    assert sample_api_request.method == "method"
+
+
+def test_parameters(sample_api_request):
+    assert sample_api_request.parameters == {"sample_field": "test", "fields": "*"}
+
+
+def test_body():
     assert 1
