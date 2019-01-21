fa38f7d1ec2e19cdbfa88faf290e58ea721672df
alex wennerberg <awennerberg@cloudbakers.com>
2019-01-20 22:04:41 -0600

    Added GitHub link to readthedocs

--- docs/conf.py
+++ docs/conf.py
@@ -84,7 +84,7 @@ html_theme = "alabaster"
 # documentation.
 #
 # html_theme_options = {}
-
+html_theme_options = {"github_user": "Cloudbakers", "github_repo": "agm"}
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
